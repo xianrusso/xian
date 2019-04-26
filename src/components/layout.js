a0eb1b403799a9f0b08c1ecfd653c45558d000d4
xian <iamchristianrusso@gmail.com>
2019-04-26 16:58:57 -0400

    add typography

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,13 +1,7 @@
-/**
- * Layout component that queries for data
- * with Gatsby's StaticQuery component
- *
- * See: https://www.gatsbyjs.org/docs/static-query/
- */
-
 import React from "react"
 import PropTypes from "prop-types"
 import { StaticQuery, graphql } from "gatsby"
+import { css } from "@emotion/core"
 
 import Header from "./header"
 import "./layout.css"
---
+++ src/utils/typography.js
@@ -0,0 +1,7 @@
+import Typography from "typography"
+import parnassus from "typography-theme-parnassus"
+
+const typography = new Typography(parnassus)
+
+export default typography
+export const rhythm = typography.rhythm
